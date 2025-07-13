e42165ec7c0323ef0132a55e4d1ea2fef0338592
MariaEloisa <mariaeloisavalenca@gmail.com>
2025-07-13 18:39:39 -0300

    Adicionando novos componentes

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -3,11 +3,12 @@ import type { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
+  color: string;
 }
 
-const Button = ({ text, ...rest }: ButtonProps) => {
+const Button = ({ text, color, ...rest }: ButtonProps) => {
   return (
-    <button className={styles.button} {...rest}>
+    <button className={styles.button} style={{ backgroundColor: color }} {...rest}>
       {text}
     </button>
   );
